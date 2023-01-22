2eebe6e16137c9a2bee8965c2bd836ef74aa9a47
Adeyeye Olalekan <adeyeye.lekan01@gmail.com>
2023-01-22 21:30:23 +0100

    Added grid layout to pages panel layout

--- src/components/PagePanel.jsx
+++ src/components/PagePanel.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import Header from "./Header";
-import Navbar from "./Navbar";
-import Footer from "./Footer";
 import '../css/PagePanel.css'
 
 const PagePanel = ({ children }) => {
   return (
     <div className="grid">
       <Header />
+      <div className="grid-row-54px"></div>
       {children}
     </div>
   );
